9c245e15de1fa540e05063449e637d594ce1a59b
dandannnnnnn <dandansandan04@gmail.com>
2024-11-13 22:00:54 +0100

    created settingErrHandler file

---
+++ settingErrHandler.h
@@ -0,0 +1,22 @@
+#define IPaddress     "tcp://192.168.0.11:1883" //IP-address:Port
+#define QoS         1
+
+#define timeMQTT_sub	10000L
+
+
+#define CLIENT    ""
+
+#define subTopic   "MB_incomingMQTT"
+#define errorTopic	SUB_TOPIC
+#define testTopic	"MB_testing"
+#define pubTopic   "MB_publishMQTT"
+
+#define topicLEN   120
+
+#define timestampLEN     80
+#define appLEN   160
+#define sevCode_LEN     5
+#define errorCode_LEN    7
+#define errorMSG_LEN		150
+#define errorParam_LEN    150
+#define errorOutput_LEN     2048
